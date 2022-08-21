89909c4b84dad91a3e477e22b74a75258d24600c
Lee-Way Wang <lee.way56@comcast.net>
2022-08-20 22:41:44 -0700

    Upload Metro map, create statistics functions
    - Update README

--- src/utilities.py
+++ src/utilities.py
@@ -1,4 +1,8 @@
 def get_formatted_path(path):
+    """
+    :param path: A list
+    :return:
+    """
     formatted_path = f"{path[0]}"
     for i in range(len(path) - 1):
         formatted_path += f" === {path[i]}"
@@ -8,4 +12,31 @@ def get_formatted_path(path):
 
 
 def print_formatted_path(formatted_path):
+    """
+    :param formatted_path:
+    :return:
+    """
     pass
+
+
+def get_metro_statistics(lines):
+    stats = dict()
+    stats["total_number_of_lines"] = len(lines)
+    stats["total_number_of_stations"] = find_total_number_of_stations(lines)
+    return stats
+
+
+def find_min_max_line_lengths(lines):
+    max_length = 0
+    # min_length =
+    for line, stations in lines:
+        number_of_stations = len(stations)
+        if number_of_stations > max_length:
+            max_length = number_of_stations
+
+
+def find_total_number_of_stations(lines):
+    total = 0
+    for line, stations in lines:
+        total += len(stations)
+    return total
